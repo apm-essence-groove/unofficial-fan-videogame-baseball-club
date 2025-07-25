c62ff058ba6d7ce2b5ebec419a9aca5e0817e060
Eeshvar Das <219355771+apmw-essence-groove@users.noreply.github.com>
2025-07-25 02:15:38 -0700

    Update league_scheduler_2.h

--- scheduling/league_scheduler_2.h
+++ scheduling/league_scheduler_2.h
@@ -9,23 +9,23 @@
 #include "game_data.h"
 
 namespace LeagueSchedulerNS {
+class LeagueScheduler2 {
+public:
+    // Main function to generate the season schedule [14, 15]
+    std::vector<ResidencyBlock> generateSeasonSchedule(const std::vector<Team>& all_teams, int games_per_team);
 
-    class LeagueScheduler2 {
-    public:
-        // Main function to generate the season schedule [26, 45]
-        std::vector<ResidencyBlock> generateSeasonSchedule(const std::vector<Team>& all_teams, int games_per_team);
-    
-    private:
-        // Helper function to create a single residency block [26, 45]
-        ResidencyBlock createResidencyBlock(const Team& host, const std::vector<Team>& visitors);
-    
-        // Implements the "alternating first bat" rule for Crossroads Games [26, 38, 39, 45, 48]
-        std::vector<Game> generateCrossroadsGames(const Team& visitor1, const Team& visitor2, const Team& host_stadium, int num_games_in_series);
-    
-        // Random number generator
-        std::mt19937 rng;
-    };
+private:
+    // Helper function to create a single residency block [14, 15]
+    ResidencyBlock createResidencyBlock(const Team& host, const std::vector<Team>& visitors);
+    // Implements the "alternating first bat" rule for Crossroads Games [15, 16]
+    std::vector<Game> generateCrossroadsGames(const Team& visitor1, const Team& visitor2, const Team& host_stadium, int num_games_in_series);
 
-}
+    // NEW: Helper function to create an Apex Residency block [6, 7]
+    ResidencyBlock createApexResidencyBlock(const Team& host, const std::vector<Team>& all_teams, int num_apex_games);
+
+    // Random number generator
+    std::mt19937 rng;
+};
+} // namespace LeagueSchedulerNS
 
 #endif // LEAGUE_SCHEDULER_2_H
